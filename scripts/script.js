3d0538f0004ee1575925fbc38e54714db119621b
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-01-13 11:49:57 +0530

    added scroll animation for cup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
-window.onload = () => {
-  console.log("Welcome");
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset /
+        (document.querySelector("#landing-section").offsetHeight -
+          window.innerHeight)
+    );
+  },
+  false
+);
+
+const revealPuddle = () => {
+  let puddle = document.querySelector("#coffee-puddle");
+  let puddleTop = puddle.getBoundingClientRect().top; //gets distance from top of viewport
+  let windowHeight = window.innerHeight; //height of viewport
+  let puddleVisible = 150; //the height at which puddle reveals
 };
