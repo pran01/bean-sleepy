94d492cbe36a040f6450e035e20c3e781b3a9bf9
Pranav Sinha <pran.sinha1.0@gmail.com>
2023-01-19 15:51:03 +0530

    added explore button and animated it

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+const exploreBtn = document.querySelector("#explore-btn");
+const drop1 = document.querySelector("#drop-1");
+const drop2 = document.querySelector("#drop-2");
+const drop3 = document.querySelector("#drop-3");
+const drop4 = document.querySelector("#drop-4");
+const coffeeFill = document.querySelector("#coffee-fill");
+const exploreText = document.querySelector("#explore-btn>span");
+
 window.addEventListener(
   "scroll",
   () => {
@@ -12,6 +20,34 @@ window.addEventListener(
   false
 );
 
+exploreBtn.addEventListener("click", () => {
+  drop1.style.display = "block";
+  drop1.classList.add("started");
+  drop2.style.display = "block";
+  drop2.classList.add("started");
+  drop3.style.display = "block";
+  drop3.classList.add("started");
+  drop4.style.display = "block";
+  drop4.classList.add("started");
+  coffeeFill.style.display = "block";
+  coffeeFill.classList.add("started");
+  exploreText.style.color = "var(--bgclr-light)";
+  setTimeout(() => {
+    drop1.style.height = "50%";
+  }, 1000);
+  setTimeout(() => {
+    drop2.style.height = "100%";
+    drop3.style.height = "100%";
+    drop4.style.height = "100%";
+  }, 2500);
+  setTimeout(() => {
+    coffeeFill.style.height = "200%";
+  }, 4000);
+  setTimeout(() => {
+    window.location = "/allRanges.html";
+  }, 5000);
+});
+
 const revealPuddle = () => {
   let puddle = document.querySelector("#coffee-puddle");
   let puddleTop = puddle.getBoundingClientRect().top; //gets distance from top of viewport
@@ -19,4 +55,12 @@ const revealPuddle = () => {
   let puddleVisible = 50; //the height at which puddle reveals
   if (puddleTop < windowHeight - puddleVisible) puddle.classList.add("active");
   else puddle.classList.remove("active");
+};
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const gotoNescafeClassic = () => {
+  window.location = "/nescafeClassic.html";
 };
